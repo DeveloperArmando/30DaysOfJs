4b7ad9a9ce613775bb3b38a5e5b0f823c81f3a29
Armando Santiago <armando.deveeloper@gmail.com>
2023-01-21 17:52:04 -0600

    initial commit
    Día 1

---
+++ datatypes.js
@@ -0,0 +1,42 @@
+let nombre_1 = 'Armando' // nombre de una persona
+let apellido_1 = 'Santiago' // apellido de una persona
+let pais_1 = 'México' // país
+let ciudad_1 = 'Cuernavaca' // ciudad capital
+let edad_1 = 24 // edad en años
+let estaCasado_1 = false // si está casado o no
+
+
+console.log(typeof nombre_1) // string
+console.log(typeof apellido_1) // string
+console.log(typeof pais_1) // string
+console.log(typeof ciudad_1) // string
+console.log(typeof edad_1) // number
+console.log(typeof estaCasado_1) // boolean
+
+
+let variable1SinValor;
+let variable2SinValor;
+let variable3SinValor;
+let variable4SinValor;
+
+let variable1ConValor = null;
+let variable2ConValor = "";
+let variable3ConValor = 0;
+let variable4ConValor = false;
+
+// declaración múltiple líneas
+let nombre_multiple_linea = 'Armando';
+let apellido_multiple_linea = 'Santiago';
+let pais_multiple_linea = 'México';
+let ciudad_multiple_linea = 'Cuernavaca';
+let edad_multiple_linea = 24;
+let estaCasado_multiple_linea = false;
+
+// declaración una sola línea
+
+let nombre_una_linea = 'Armando', apellido_una_linea = 'Santiago', pais_una_linea = 'México', ciudad_una_linea = 'Cuernavaca', edad_una_linea = 24, estaCasado_una_linea = false;
+
+let miEdad = 24;
+let tuEdad = 30;
+
+console.log(`Tengo ${miEdad} años.\nTú tienes ${tuEdad} años`)
---
+++ variable.js
@@ -0,0 +1,15 @@
+// "los comentarios pueden hacer que el código sea legible"
+// "Bienvenido a 30DaysOfJavaScript"
+
+/*
+Los comentarios pueden hacer el código legible, fácil de usar e informativo"
+*/ 
+
+
+// Declarando diferentes variables de diferentes tipos de datos
+let nombre = 'Armando' // nombre de una persona
+let apellido = 'Santiago' // apellido de una persona
+let pais = 'México' // país
+let ciudad = 'Cuernavaca' // ciudad capital
+let edad = 24 // edad en años
+let estaCasado = false // si está casado o no
